6d00f0285d85f46c69ce785d43671e7ded1d2220
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-04-05 23:41:59 +0530

    Add initial implementation of Alien class and notes on constructor usage

---
+++ OOPS-2/index.js
@@ -0,0 +1,48 @@
+
+
+
+
+// class Alien { // Name of the class
+//     // The constructor method will take a number of parameters and assign those parameters as properties to the created object.
+//     constructor (name, phrase) {
+//         this.name = name
+//         this.phrase = phrase
+//         this.species = "alien"
+//     }
+//     // These will be the object's methods.
+//     fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+//     sayPhrase = () => console.log(this.phrase)
+// }
+
+
+// // a constructor is a function which is used to initialization on object 
+// const superman = new Alien("DC", "I'm DC alien!") //1st object
+// const batman = new Alien("BAT ", "Nolan") //2nd object
+
+
+
+// console.log(superman.name) 
+
+// console.log(superman.phrase) 
+// superman.fly() 
+// superman.sayPhrase() 
+
+// batman.fly() 
+
+
+
+
+
+
+
+
+
+/// QS :-
+ // Write a class of your choices . make a constructor inside it and pass some parameters
+ // to initilaze the object .make object using that class
+
+
+
+
+
+

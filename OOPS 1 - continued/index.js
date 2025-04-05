ce1aeed1db9fcc787f989bdec52a35eac3243a5c
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-04-05 15:00:45 +0530

    Refactor myFilter implementation and add class examples for OOP concepts

--- OOPS 1 - continued/index.js
+++ OOPS 1 - continued/index.js
@@ -70,39 +70,171 @@
 
 
 
-Array.prototype.myFilter = function(callback) {
-    var arr = [];
+// Array.prototype.myFilter = function(callback) {
+//     var arr = [];
  
  
-    for (let i = 0; i < this.length; i++) {
-        if (callback.call(this,this[i])) {
-            arr.push(this[i]);
-        }
+//     for (let i = 0; i < this.length; i++) {
+//         if (callback.call(this,this[i])) {
+//             arr.push(this[i]);
+//         }
+//     }
+//     return arr;
+//  }
+ 
+ 
+//  const arr=["elephant","tiger","bear","lion","peacock","vulture","monkey"];
+ 
+ 
+//  const ans = arr.myFilter((ele) => {
+//     return (ele.length % 2 === 0);///even
+//  });
+ 
+ 
+// const ans1 =arr.myFilter((ele)=>{
+//     return(ele.length %2 ===1) //odd
+// })
+
+// console.log(`Even ---> ${ans}
+//     ODD --> ${ans1}`)
+
+
+
+
+
+
+
+
+
+///////////////////   2.00.00 hr  all plloyfil reviseion must be watch 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////    OOPS START ///////////////
+
+
+
+
+
+
+
+
+
+///  ex:-
+
+
+// const alien1 = {
+//     name: "Ali",
+//     species: "alien",
+//     phrase: () => console.log("I'm Ali the alien!"),
+//     fly: () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+// }
+// const alien2 = {
+//     name: "Lien",
+//     species: "alien",
+//     sayPhrase: () => console.log("Run for your lives!"),
+//     fly: () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+// }
+// const bug1 = {
+//     name: "Buggy",
+//     species: "bug",
+//     sayPhrase: () => console.log("Your debugger doesn't work with me!"),
+//     hide: () => console.log("You can't catch me now!")
+// }
+
+
+// const bug2 = {
+//     name: "Erik",
+//     species: "bug",
+//     sayPhrase: () => console.log("I drink decaf!"),
+//     hide: () => console.log("You can't catch me now!")
+// }
+// const Robot1 = {
+//     name: "Tito",
+//     species: "robot",
+//     sayPhrase: () => console.log("I can cook, swim and dance!"),
+//     transform: () => console.log("Optimus prime!")
+// }
+// const Robot2 = {
+//     name: "Terminator",
+//     species: "robot",
+//     sayPhrase: () => console.log("Hasta la vista, baby!"),
+//     transform: () => console.log("Optimus prime!")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Alien { // Name of the class
+    // The constructor method will take a number of parameters and assign those parameters as properties to the created object.
+    constructor (name, phrase) {
+        this.name = name
+        this.phrase = phrase
+        this.species = "alien"
     }
-    return arr;
- }
- 
- 
- const arr=["elephant","tiger","bear","lion","peacock","vulture","monkey"];
- 
- 
- const ans = arr.myFilter((ele) => {
-    return (ele.length % 2 === 0);///even
- });
- 
- 
-const ans1 =arr.myFilter((ele)=>{
-    return(ele.length %2 ===1) //odd
-})
+    // These will be the object's methods.
+    fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+    sayPhrase = () => console.log(this.phrase)
+}
 
-console.log(`Even ---> ${ans}
-    ODD --> ${ans1}`)
 
+
+
+class Bug {
+    constructor (name, phrase) {
+        this.name = name
+        this.phrase = phrase
+        this.species = "bug"
+    }
+    hide = () => console.log("You can't catch me now!")
+    sayPhrase = () => console.log(this.phrase)
+}
 
 
 
 
 
+class Robot {
+    constructor (name, phrase) {
+        this.name = name
+        this.phrase = phrase
+        this.species = "robot"
+    }
+    transform = () => console.log("Optimus prime!")
+    sayPhrase = () => console.log(this.phrase)
+}
+//a constructor is a function which is used to initialize an object
 
 
 
@@ -115,4 +247,5 @@ console.log(`Even ---> ${ans}
 
 
 
+//////////   complete /////////
 
--- Polyfills-2/notes.md
+++ Polyfills-2/notes.md
@@ -75,3 +75,140 @@ console.log(result);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////     OOPS START     ///////////////
+
+
+
+
+
+
+Q)     What is object-oriented programming?
+--->    Object-oriented programming (OOP) is a computer programming model that organises software design around data, or objects, rather than functions and logic. An object can be defined as a data field that has unique attributes and behaviour.
+
+
+
+
+ex:-
+
+
+const alien1 = {
+    name: "Ali",
+    species: "alien",
+    phrase: () => console.log("I'm Ali the alien!"),
+    fly: () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+}
+const alien2 = {
+    name: "Lien",
+    species: "alien",
+    sayPhrase: () => console.log("Run for your lives!"),
+    fly: () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+}
+const bug1 = {
+    name: "Buggy",
+    species: "bug",
+    sayPhrase: () => console.log("Your debugger doesn't work with me!"),
+    hide: () => console.log("You can't catch me now!")
+}
+
+
+const bug2 = {
+    name: "Erik",
+    species: "bug",
+    sayPhrase: () => console.log("I drink decaf!"),
+    hide: () => console.log("You can't catch me now!")
+}
+const Robot1 = {
+    name: "Tito",
+    species: "robot",
+    sayPhrase: () => console.log("I can cook, swim and dance!"),
+    transform: () => console.log("Optimus prime!")
+}
+const Robot2 = {
+    name: "Terminator",
+    species: "robot",
+    sayPhrase: () => console.log("Hasta la vista, baby!"),
+    transform: () => console.log("Optimus prime!")
+}
+
+
+
+
+
+
+
+
+
+-->  See that all characters have the name and species properties and also the sayPhrase method. Moreover, each species has a method that belongs only to that species (for example, aliens have the fly method).
+
+-->  To solve this problem we need a programmatic way of creating objects and setting different properties and methods given a set of conditions. And that's what classes are good for. 😉
+
+
+
+
+
+
+
+
+
+
+
+--->   Classes set a blueprint to create objects with predefined properties and methods. By creating a class, you can later on instantiate (create) objects from that class, that will inherit all the properties and methods that class has.
+
+
+
+
+
+
+
+
+class Alien { // Name of the class
+    // The constructor method will take a number of parameters and assign those parameters as properties to the created object.
+    constructor (name, phrase) {
+        this.name = name
+        this.phrase = phrase
+        this.species = "alien"
+    }
+    // These will be the object's methods.
+    fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+    sayPhrase = () => console.log(this.phrase)
+}
+
+class Bug {
+    constructor (name, phrase) {
+        this.name = name
+        this.phrase = phrase
+        this.species = "bug"
+    }
+    hide = () => console.log("You can't catch me now!")
+    sayPhrase = () => console.log(this.phrase)
+}
+
+class Robot {
+    constructor (name, phrase) {
+        this.name = name
+        this.phrase = phrase
+        this.species = "robot"
+    }
+    transform = () => console.log("Optimus prime!")
+    sayPhrase = () => console.log(this.phrase)
+}
+//a constructor is a function which is used to initialize an object
+
+
+
+
+
+
+

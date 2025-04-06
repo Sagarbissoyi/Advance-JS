91e720ca848dd73781b2bcf53e90820990ff3185
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-04-07 00:25:05 +0530

    updates notes

--- OOPS-2/index.js
+++ OOPS-2/index.js
@@ -37,12 +37,83 @@
 
 
 
+
+
+
+
+
+
+
+// class Alien { // Name of the class
+//     // The constructor method will take a number of parameters and assign those parameters as properties to the created object.
+//     constructor (name, phrase) {
+//         this.name = name
+//         this.phrase = phrase
+//         this.species = "alien"
+//     }
+//     // These will be the object's methods.
+//     fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+//     sayPhrase = () => console.log(this.phrase)
+// }
+
+
+
+// const superhero = new Alien("Ali", "Ali the alien") //1st object
+
+
+// console.log(superhero)
+
+// superhero.fly()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /// QS :-
  // Write a class of your choices . make a constructor inside it and pass some parameters
  // to initilaze the object .make object using that class
+// try to print all the properties using back ticks with string interpolation
 
 
 
 
+class nolan{
+    constructor (teddy,statement,verdict){
+        this.name= teddy;
+        this.phase = statement;
+        this.species = "Superhero";
+        this.power = "100";
+        this.fly = verdict;
+    }
+    printAll = () => {
+        console.log(`${this.name} ${this.phase} ${this.species} ${this.power} ${this.fly}`)
+    }
 
 
+}
+
+
+const superman = new nolan ("DC","SRK","Yes");
+
+const batman = new nolan("Bat","Nolan","No");
+
+const ironman = new nolan("Iron","Steve","Yes")
+
+
+
+superman.printAll()
+batman.printAll();
+ironman.printAll()
